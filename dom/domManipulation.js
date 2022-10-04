9060560638d6bff9edae46229109224145171aaa
EllenSouza <ellen.almeida.souza21@gmail.com>
2022-10-03 22:19:00 -0300

    change ordenation and fix space amoung minions

--- dom/domManipulation.js
+++ dom/domManipulation.js
@@ -20,39 +20,9 @@ div2.id = "explanationText"
 body.appendChild(div2)
 
 let p = document.createElement("p")
-p.textContent = "This is a small application of DOM manipulation using JavaScript. When you click on any of minions images, it disappears and to make it appear again: CTRL + m"
+p.textContent = "This is a small application of DOM manipulation using JavaScript. When you click on any of minions images, it disappears. To make all appear again: CTRL + m"
 p.style.textAlign ="justify"
 div2.appendChild(p)
-
-//button to add more minions in the screen
-
-let button = document.createElement("button")
-button.setAttribute("id", "addMinions")
-button.textContent = "ADD Minion"
-body.appendChild(button)
-button.setAttribute("onclick", "addImg()")
-
-var cont=0;
-function addImg (){
-    let minionImage = document.createElement("img")
-    minionImage.setAttribute("src", "../dom/img/evil-minions.jpg")
-    minionImage.setAttribute("alt" , "evil-minion")
-    minionImage.style.height = "7em"
-    minionImage.style.width = "7em"
-    minionImage.style.borderRadius = "45%"
-    minionImage.style.marginRight = "2.5em"
-    minionImage.style.marginLeft = "2.5em"
-
-    minionImage.id = `evilMinion${cont}`
-    cont++
-    body.appendChild(minionImage)
-
-    minionImage.onclick = function(){
-        minionImage.style.display = "none"
-    }
-
-}
-
 
 
 //images
@@ -66,7 +36,6 @@ figure = document.createElement("figure")
 div3.appendChild(figure)
 
 div3.style.display = "flex"
-//div3.style.justifyContent = "space-around"
 
 img1 = document.createElement("img")
 img1.setAttribute("src" , "../dom/img/stuart.jpeg")
@@ -123,16 +92,16 @@ figure4.appendChild(img4)
 //make minions disappear
 
 img1.onclick = function (){
-    img1.style.display = "none"
+    figure.style.display = "none"
 }
 img2.onclick = function (){
-    img2.style.display = "none"
+    figure2.style.display = "none"
 }
 img3.onclick = function (){
-    img3.style.display = "none"
+    figure3.style.display = "none"
 }
 img4.onclick = function (){
-    img4.style.display = "none"
+    figure4.style.display = "none"
 }
 
 
@@ -140,9 +109,63 @@ img4.onclick = function (){
 //make all minions visible again 
 window.onkeydown = function (event){
     if (event.ctrlKey && event.key == "m"){
-        img1.style.display = "block"
-        img2.style.display = "block"
-        img3.style.display = "block"
-        img4.style.display = "block"
+        figure.style.display = "block"
+        figure2.style.display = "block"
+        figure3.style.display = "block"
+        figure4.style.display = "block"
     }
+}
+
+//evil-minions
+
+let sectionEvilMinions = document.createElement("section")
+sectionEvilMinions.id = "sectionEvilMinions"
+let h2 = document.createElement("h2")
+h2.textContent = "Adding Evil Minions"
+h2.style.textAlign = "center"
+
+sectionEvilMinions.appendChild(h2)
+
+let p2 = document.createElement("p")
+p2.style.textAlign = "justified"
+p2.textContent = "Click on the button to add Evil Minions to the screen. Click on the image of each one minions and it desappears"
+
+sectionEvilMinions.appendChild(p2)
+
+body.appendChild(sectionEvilMinions)
+
+//button to add more minions in the screen
+let button = document.createElement("button")
+button.setAttribute("id", "addMinions")
+button.textContent = "ADD Minion"
+sectionEvilMinions.appendChild(button)
+button.setAttribute("onclick", "addImg()")
+
+
+
+var sectionImgEvilMinions = document.createElement("section")
+sectionImgEvilMinions.id = "imgEvilMinions"
+
+body.appendChild(sectionImgEvilMinions)
+
+var cont=0;
+function addImg (){
+    let minionImage = document.createElement("img")
+    minionImage.setAttribute("src", "../dom/img/evil-minions.jpg")
+    minionImage.setAttribute("alt" , "evil-minion")
+    minionImage.style.height = "7em"
+    minionImage.style.width = "7em"
+    minionImage.style.borderRadius = "45%"
+    minionImage.style.marginRight = "2.5em"
+    minionImage.style.marginTop = "2.5em"
+    minionImage.style.marginLeft = "2.5em"
+
+    minionImage.id = `evilMinion${cont}`
+    cont++
+    sectionImgEvilMinions.appendChild(minionImage)
+
+    minionImage.onclick = function(){
+        minionImage.style.display = "none"
+    }
+
 }
